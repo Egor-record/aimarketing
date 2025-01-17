53b46df6fbd0112254af7e15c9390c1620687531
Egor Siaskov <egor.siaskov@telekom.com>
2025-01-17 23:00:54 +0200

    added path to dist

--- api.js
+++ api.js
@@ -1,18 +1,16 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const { getAllUsers } = require('./db.js')
+const PORT = 3700;
 
 const initAPI = () => {
   const app = express();
-  const PORT = 3700;
   app.use(cors());
+
   const apiRouter = express.Router();
 
   app.use(express.json());
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
 
   apiRouter.get('/users', async (_,res) => {
     const users = await getAllUsers()
@@ -20,6 +18,16 @@ const initAPI = () => {
   });
 
   app.use('/api/v1', apiRouter);
+
+  app.use(express.static(path.join(__dirname, '/front/ai-admin/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/front/ai-admin/dist', 'index.html'));
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 }
 
 module.exports = { initAPI };
